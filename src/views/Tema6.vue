32e0d6b6c307c6171a54e81a2715c99b59e332b5
Fabio Fonseca <fabiofonseca.arg@gmail.com>
2026-05-25 05:22:42 -0500

    Tema 6

--- src/views/Tema6.vue
+++ src/views/Tema6.vue
@@ -1,12 +1,145 @@
 <template lang="pug">
-  .curso-main-container.pb-3
-    BannerInterno
-    .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
-      .titulo-principal.color-acento-contenido
-        .titulo-principal__numero
-          span 6
-        h1 Paso a paso del armado técnico
-  
+.curso-main-container.pb-3
+  BannerInterno
+  .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
+    .titulo-principal.color-acento-contenido
+      .titulo-principal__numero
+        span 6
+      h1 6	Procedimiento técnico de arreglo de cama (tendido de precisión)
+
+    separador
+    
+    #t_6_2.titulo-segundo.color-acento-contenido(data-aos="flip-up")
+      h2 6.2 Paso a paso del armado técnico
+
+    .mt-5.pt-4
+      .fullwidth-bg.fullwidth-bg--1
+      img(src="@/assets/curso/tema6/1.png", alt="" data-aos="zoom-in-up").z-2
+      
+      .row.justify-content-center.z-2.mb-3
+        .col-lg-10.p-4.drop-shadow(data-aos="fade-down")
+          div.banner-text.shadow
+            p.mb-0 El paso a paso del armado técnico corresponde a la secuencia organizada de actividades que se realizan para efectuar correctamente el tendido y presentación de la cama en una habitación de alojamiento. Este procedimiento se desarrolla siguiendo estándares técnicos de higiene, orden, ergonomía y calidad, con el propósito de garantizar comodidad, funcionalidad y una adecuada presentación del espacio destinado al huésped.
+
+    .row.justify-content-center.mb-5
+      .col-12.col-lg-10.mb-0.col-md-12
+        .cita-decorada
+          blockquote.cita-decorada__caja.p-3.bg-acento-contenido-a5.rounded-4
+            .row.justify-content-center.align-items-center
+              .col-12.col-lg-3.mb-3.mb-lg-0
+                img(src="@/assets/curso/common/newspaper.png", class="img-fluid cita-decorada__imagen-flotante-4 cita-decorada__imagen-flotante-4--bigger")
+              .col-12.col-lg-6.cita-decorada__texto.mb-4.mb-lg-0
+                p.mb-0.text-center.text-lg-start #[strong Anexo]. Secuencia Técnica del tendido
+              .col-lg-3.col-md-3.col-12.d-flex.justify-content-center
+                a.boton.color-acento-botones.my-2(:href="obtenerLink('/downloads/8_Secuencia_Tecnica_del_tendido.pdf')" target="_blank")
+                  span Descargar
+                  i.fas.fa-file-download
+
+    separador
+    
+    #t_6_3.titulo-segundo.color-acento-contenido(data-aos="flip-up")
+      h2 6.3	Especificaciones técnicas de la lencería de cama
+
+    .row.g-0.rounded-4.bg-acento-botones-a5.mb-5.justify-content-center
+      .col-sm-6.col-lg-3.d-flex
+        figure.m-0
+          img(src="@/assets/curso/tema6/2.png", alt="alt text" data-aos="zoom-in-down")
+      .col-lg-9.p-4.p-xl-0
+        .d-flex.flex-column.justify-content-center.h-100
+          p.px-lg-3 La lencería debe tener un "margen de caída" suficiente para permitir el remetido técnico (ángulo de mitra) y cubrir el espesor del colchón, que en hotelería suele oscilar entre los 25 y 30 cm. Según García y López (2023), "una lencería con dimensiones insuficientes impide la tensión necesaria para un tendido de alta calidad y acelera el deterioro de las costuras por exceso de fuerza" (p. 195).
+
+    .row.justify-content-center.align-items-center.mb-4.mb-lg-5.z-2
+      .col-lg-8
+        .bloque-texto-e.bg-acento-contenido-a5.py-2.px-4.rounded-4
+          .bloque-texto-e__texto
+            i.fas.fa-quote-left
+            p una lencería con dimensiones insuficientes impide la tensión necesaria para un tendido de alta calidad y acelera el deterioro de las costuras por exceso de fuerza
+            i.fas.fa-quote-right
+          .row.align-items-end
+            .col
+              .bloque-texto-e__autor 
+                p.mb-0 (Boullón, 2006).
+      .col-sm-8.col-lg-4
+        figure
+          img(data-aos="zoom-in-down", src="@/assets/curso/tema6/3.png", alt="Especificaciones técnicas de la lencería de cama")
+    
+    .bg-title.bg-title--lg.p-3.rounded-5.pe-5.mb-4(data-aos="fade-down-right")
+      h5.mb-0 Tabla de Medidas estándar (colchón vs. lencería)
+
+    .p-4.rounded-4.bg-secundario-s5.mb-5
+      p.mb-0.text-center Para efectos pedagógicos en el SENA, utilizaremos la siguiente tabla comparativa que detalla la medida del mueble frente a la medida ideal de la sábana.
+
+    .row.justify-content-center.mb-5
+      .col-xl-10
+        .titulo-sexto.color-acento-contenido.mb-4
+          h5 Tabla 7.
+          span Dimensiones de lencería según tipología de cama
+        .tabla-a.color-acento-contenido.tabla-a--text-left
+          table
+            caption Nota. SENA, (2024).
+            thead
+              tr(style="background-color: #F0F6F9")
+                th Tipo de cama
+                th Medida colchón (cm)
+                th Sábana (ancho x largo cm)
+                th Tamaño de almohadas
+            tbody
+              tr
+                td Sencilla (#[em Twin]).
+                td 90 - 100 x 190
+                td 180 x 280
+                td Estándar (50 x 70 cm).
+              tr
+                td Semidoble.
+                td 120 x 190
+                td 200 x 280
+                td Estándar (50 x 70 cm).
+              tr
+                td Doble (full).
+                td 140 x 190
+                td 220 x 280
+                td Estándar (50 x 75 cm).
+              tr
+                td Queen size.
+                td 160 x 190 / 200
+                td 240 x 290
+                td Queen (50 x 80 cm).
+              tr
+                td King size
+                td 200 x 200
+                td 280 x 290
+                td King (50 x 90 cm).
+
+    .bg-title.bg-title--lg.p-3.rounded-5.pe-5.mb-4(data-aos="fade-down-right")
+      h5.mb-0 Componentes adicionales y sus medidas
+
+    .p-4.rounded-4.bg-secundario-s5.mb-5
+      p.mb-0.text-center Además de las sábanas, el coordinador debe supervisar que los complementos mantengan la misma proporción:
+
+    .row.justify-content-center.align-items-stretch.mb-5
+      .col-sm-6.col-lg-4.mb-4.mb-xl-0
+        .tarjeta.bg-img.bg-img--3.p-4.py-5.h-100
+          .row.justify-content-center.mb-3
+            .col-6
+              img.w-90.mb-4(src='@/assets/curso/tema6/4.svg' alt='')
+          h5.text-center Protector de colchón: 
+          p.text-center.mb-4 Debe ser de la medida exacta del colchón (ej. 160 x 190 para Queen) y contar con un faldón elástico de al menos 30 cm para abrazar toda la estructura.
+      .col-sm-6.col-lg-4.mb-4.mb-xl-0
+        .tarjeta.bg-img.bg-img--3.p-4.py-5.h-100
+          .row.justify-content-center.mb-3
+            .col-6
+              img.w-90.mb-4(src='@/assets/curso/tema6/5.svg' alt='')
+          h5.text-center Mantas y edredones: 
+          p.text-center.mb-4 Deben ser entre 30 y 40 cm más anchos que el colchón para que la caída lateral cubra el espesor de este y el borde de la base.
+
+      .col-sm-6.col-lg-4.mb-4.mb-sm-0
+        .tarjeta.bg-img.bg-img--3.p-4.py-5.h-100
+          .row.justify-content-center.mb-3
+            .col-6
+              img.w-90.mb-4(src='@/assets/curso/tema6/6.svg' alt='')
+          h5.text-center El Embozo (doblado superior): 
+          p.text-center.mb-4 Independientemente del tamaño, el estándar de hotelería dicta que el embozo debe tener un ancho de 15 a 20 cm, permitiendo que luzca la marca o el diseño de la sábana encimera.
+      
 </template>
 
 <script>
